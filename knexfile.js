1d1e814d528630e65c281d6a4811e7f76a311d9e
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-10 15:50:40 +0700

    adding knex config file

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+require('./system/config').run();
+
+module.exports = {
+
+  development: {
+    client: process.env.database.driver,
+    connection: {
+      user:     process.env.database.user,
+      password: process.env.database.pass,
+      database: process.env.database.name
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
