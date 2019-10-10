78401db1224b26c59fd7adbb78b350a8207d97ea
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-10 15:12:41 +0700

    wrapping system action with a function

--- system/index.js
+++ system/index.js
@@ -1,8 +1,19 @@
 const load = require('./helpers/load');
+const bodyParser = require('body-parser');
 
-load.filesIn(__dirname, {
-  exclude: __filename,
-})
-.forEach(file => {
-  require(`./${file}`).run();
-});
+const init = (app) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // loading all system files
+  load.filesIn(__dirname, {
+    exclude: __filename,
+  })
+  .forEach(file => {
+    require(`./${file}`).run(app);
+  });
+};
+
+module.exports = {
+  init
+};
