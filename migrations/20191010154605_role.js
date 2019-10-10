0c42ed1c34fb178f2015c7252fbc384a1eabec7e
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-10 16:42:21 +0700

    fixing migration example

--- migrations/20191010154605_role.js
+++ migrations/20191010154605_role.js
@@ -1,7 +1,13 @@
+const tableName = 'roles';
+
 exports.up = function(knex) {
-  
+  return knex.schema.createTable(tableName, (table)=>{
+    table.increments();
+    table.string('title').notNullable().unique();
+    table.timestamps();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable(tableName);
 };
