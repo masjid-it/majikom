298ec062a7fee885710ba69a6e6b13adcae09d79
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-10 14:59:22 +0700

    starting app

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const system = require('./system/index');
 
-
-
-// app.listen();
+app.listen(process.env.app.port, () => {
+  console.log(`App run on port ${process.env.app.port}`);
+});
