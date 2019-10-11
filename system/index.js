fb4032de3ee493d0f9088ba5622e0f15438d56ba
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-11 18:52:14 +0700

    register static directory

--- system/index.js
+++ system/index.js
@@ -1,7 +1,9 @@
 const load = require('./helpers/load');
 const bodyParser = require('body-parser');
+const express = require('express');
 
 const init = (app) => {
+  app.use(express.static(`${__dirname}/../public`));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
 
