05e2d6250e313579a7db3a2bf79de651361db7d9
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-18 11:44:45 +0700

    create user controller

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const user = require('../repository/user');
+const role = require('../repository/role');
+const auth = require('../middleware/auth');
 
-router.get('/users', (req, res) => {
-  res.send('users data');
+router.get('/users', auth, async (req, res) => {
+  res.render('user', { users: await user.all() });
+});
+
+router.get('/users/add', auth, async (req, res) => {
+  res.render('user/add', { roles: await role.all() });
+});
+
+router.post('/users/add', auth, async (req, res) => {
+  const { email, password, role } = req.body;
+  await user.create({
+    email,
+    password,
+    role_id: role,
+  });
+  res.redirect('/users');
 });
 
 module.exports = router;
