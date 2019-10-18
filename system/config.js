5a4e6032b0a377569878c047684b4cb080d913fb
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2019-10-18 13:05:01 +0700

    configure express session

--- system/config.js
+++ system/config.js
@@ -1,9 +1,10 @@
+const session = require('express-session');
 require('dotenv').config();
 const load = require('./helpers/load');
 
 const userConfigDir = `${__dirname}/../config`;
 
-const run = () => {
+const run = (app) => {
   const files = load.filesIn(userConfigDir, { exclude: `${userConfigDir}/index.js` });
   files.forEach(file => {
     const config = require(`${userConfigDir}/${file}`);
@@ -12,6 +13,15 @@ const run = () => {
       ...config,
     }
   });
+
+  app.use(session({
+    secret: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production'
+    },
+  }));
 };
 
 module.exports = {
